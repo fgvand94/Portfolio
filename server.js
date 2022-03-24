bf21834e32cf385eee5eff9966df441532c66783
Forrest Van Duren <forrestvanduren1@gmail.com>
2022-03-24 16:36:33 -0600

    ...

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
 const express = require("express");
 
 const nodemailer = require("nodemailer");
-/*const multiparty = require("multiparty");
-const { getMaxListeners } = require("process");
-require("dotenv").config();
-*/
-// instantiate an express app
+
+
 const app = express();
 
-//make the contact page the first page on the app
-// the app.use is what actually makes everything from the webpage show up in your server
+
 app.use(express.static('public'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "public/index.html")
 });
-/*I'm thinking maybe this app.get res.sendFile thing is how links transfer you to differnt
-webpages or parts on the site without showing a new .html at the end of the page. you just*/
 
-/*actually commenting the above out still shows the webpage it's the app.use doing that
-I'm going to reload the video and see what he said it was for again after I get the email
-working.*/
 
 
 app.post('/', (req, res) => {
@@ -39,7 +30,6 @@ app.post('/', (req, res) => {
     from: req.body.email,
     to: 'portfolliotemp@gmail.com',
     subject: `message from ${req.body.name}: ${req.body.email}`,
-    //subject
     text: req.body.message,
 
   }
@@ -62,59 +52,3 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
 
-/*It's interesting cause this actually sends the message from your own email address.
-I was thinking it would send it from the other persons but now thinking about it the form
-would have to directly acess the persons email essentially to send it from their email so
-I don't know if that really possible unless they provide you with their password which isn't
-going to happen lawl. I could just have a test email that it's sent to so my password for my
-real account isn't out in the open and then have all the emails sent to the test email form
-the test email forwarded to my actual account. I could probably automate replies to the email
-that sent it to me though possibly. */
-
-/*
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com", //replace with your email provider
-    port: 587,
-    auth: {
-      user: process.env.portfolliotemp@gmail.com,
-      pass: process.env.fourothreepm10!,
-    },
-  });
-
-  transporter.verify(function (error, success) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Server is ready to take our messages");
-    }
-  });
-
-  app.post("/send", (req, res) => {
-  //1.
-  let form = new multiparty.Form();
-  let data = {};
-  form.parse(req, function (err, fields) {
-    console.log(fields);
-    Object.keys(fields).forEach(function (property) {
-      data[property] = fields[property].toString();
-    });
-
-    //2. You can configure the object however you want
-    const mail = {
-      from: data.name,
-      to: process.env.EMAIL,
-      subject: data.subject,
-      text: `${data.name-input} <${data.email-input}> \n${data.message-input}`,
-    };
-
-    //3.
-    transporter.sendMail(mail, (err, data) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Something went wrong.");
-      } else {
-        res.status(200).send("Email successfully sent to recipient!");
-      }
-    });
-  });
-});*/
