0a5bbf84f4040287edc4915eafb5a20ea8cdd1cb
Forrest Van Duren <forrestvanduren1@gmail.com>
2022-03-27 00:37:51 -0600

    test

--- server.js
+++ server.js
@@ -6,17 +6,19 @@ const nodemailer = require("nodemailer");
 const app = express();
 
 
-app.use(express.static('public'));
-app.use(express.json());
+// app.use(express.static('public'));
+// app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "public/index.html")
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + "public/index.html")
+// });
 
 
 
 
 app.post('/', (req, res) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", "*");
   console.log(req.body);
 
   const transporter = nodemailer.createTransport ({
